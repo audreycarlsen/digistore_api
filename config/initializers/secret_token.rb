0dabeda2274fe7258ba9f9406a82fa63467c70d6
Audrey Carlsen <audreycarlsen@gmail.com>
2014-03-18 09:14:35 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DigistoreApi::Application.config.secret_key_base = '5f80dd2576102d7befb1e0e9b174708ccb0af88c3de1e3e80d08a482b17266dd3969592951284ec69eef5dcf0f13b9001d217f356274b2d022732fe67c4ee999'
