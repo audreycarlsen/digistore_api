744f0ac5f2d54f38a056487c48a312a3ad26e815
Audrey Carlsen <audreycarlsen@gmail.com>
2014-03-20 12:30:18 -0700

    actionmailer view works, small blessings

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,7 +14,8 @@ class OrdersController < ApplicationController
       email:    order_params[:email],
       zipcode:  order_params[:zipcode],
       exp_date: order_params[:exp_date],
-      cvv:      order_params[:cvv])
+      cvv:      order_params[:cvv],
+      status:   order_params[:status])
 
     if @order.save
       OrderMailer.confirmation(@order.id).deliver
@@ -27,6 +28,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:cart, :name, :email, :zipcode, :exp_date, :cvv, :cc_number)
+    params.require(:order).permit(:status, :cart, :name, :email, :zipcode, :exp_date, :cvv, :cc_number)
   end
 end
